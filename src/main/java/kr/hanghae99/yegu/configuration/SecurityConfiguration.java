f61741262ce104fccd051d61cf56828e7b13a85b
Seong-Gook Lim <sklim0932@gmail.com>
2024-04-25 01:48:56 +0900

    feature: Security Configuration 작성
    계정 비밀번호 암호화용 PasswordEncoder 빈과 이름, 주소 등 복호화가 필요한 정보에 대한 AesBytesEncryptor 빈 생성

---
+++ src/main/java/kr/hanghae99/yegu/configuration/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package kr.hanghae99.yegu.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Value("${symmetric.key}")
+    private String symmetricKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .httpBasic(configurer -> configurer.disable())
+                .formLogin(configurer -> configurer.disable())
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AesBytesEncryptor aesBytesEncryptor() {
+        return new AesBytesEncryptor(symmetricKey, "89858283837164");
+    }
+}
