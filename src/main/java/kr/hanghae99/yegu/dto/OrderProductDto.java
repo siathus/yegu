ac90feb0b48b92812910513808aaee09440dedce
Seong-Gook Lim <sklim0932@gmail.com>
2024-04-26 10:49:07 +0900

    feature: 주문 요청 DTO OrderProductDto, OrderRequestDto에 생성자 및 Setter 설정

--- src/main/java/kr/hanghae99/yegu/dto/OrderProductDto.java
+++ src/main/java/kr/hanghae99/yegu/dto/OrderProductDto.java
@@ -2,11 +2,14 @@ package kr.hanghae99.yegu.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
 @NoArgsConstructor
-@Getter
+@Getter @Setter
 public class OrderProductDto {
     @NotNull
     private Long productId;
--- src/main/java/kr/hanghae99/yegu/dto/OrderRequestDto.java
+++ src/main/java/kr/hanghae99/yegu/dto/OrderRequestDto.java
@@ -3,12 +3,13 @@ package kr.hanghae99.yegu.dto;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @NoArgsConstructor
-@Getter
+@Getter @Setter
 public class OrderRequestDto {
     @NotNull
     private Long userId;
