9df4c77d8d1adae71d26dae491bf5da65f43605c
Seong-Gook Lim <sklim0932@gmail.com>
2024-04-29 18:57:43 +0900

    feature: 주문 상품 및 반품 상태 자동화를 위한 Scheduler 설정

--- src/main/java/kr/hanghae99/yegu/YeguApplication.java
+++ src/main/java/kr/hanghae99/yegu/YeguApplication.java
@@ -3,7 +3,9 @@ package kr.hanghae99.yegu;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableJpaAuditing
 @SpringBootApplication
 public class YeguApplication {
