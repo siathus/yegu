f034fcc7e6ea7e71d84538ec065f505a3c5dfcfa
Seong-Gook Lim <sklim0932@gmail.com>
2024-04-29 11:17:18 +0900

    feature: Security Configuration 수정 - 테스트를 위해 CSRF 방지 비활성화

--- src/main/java/kr/hanghae99/yegu/configuration/SecurityConfiguration.java
+++ src/main/java/kr/hanghae99/yegu/configuration/SecurityConfiguration.java
@@ -1,17 +1,21 @@
 package kr.hanghae99.yegu.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-@EnableWebSecurity
+//@EnableWebSecurity
 @Configuration
 public class SecurityConfiguration {
 
@@ -21,8 +25,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+                .csrf(CsrfConfigurer<HttpSecurity>::disable)
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
                 .httpBasic(configurer -> configurer.disable())
                 .formLogin(configurer -> configurer.disable())
                 .build();
