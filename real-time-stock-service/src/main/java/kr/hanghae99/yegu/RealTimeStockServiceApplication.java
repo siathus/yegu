c3e3ff3d710330932d92bf43cd166486e1c6ef5e
Seong-Gook Lim <sklim0932@gmail.com>
2024-05-13 20:45:25 +0900

    feature: 실시간 재고 관리 서비스를 위한 모듈 추가

---
+++ real-time-stock-service/src/main/java/kr/hanghae99/yegu/RealTimeStockServiceApplication.java
@@ -0,0 +1,13 @@
+package kr.hanghae99.yegu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeStockServiceApplication.class, args);
+	}
+
+}
---
+++ real-time-stock-service/src/main/java/kr/hanghae99/yegu/configuration/RedisConfiguration.java
@@ -0,0 +1,9 @@
+package kr.hanghae99.yegu.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfiguration {
+
+}
